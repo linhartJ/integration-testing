2dd8a5d129d39c99653d483b8d293c865254979e
j.linhart <linhartos@centrum.cz>
2018-11-18 10:30:38 +0100

    added spring extension

--- cr-tool/src/test/kt/tool/TestToolConfiguration.kt
+++ cr-tool/src/test/kt/tool/TestToolConfiguration.kt
@@ -2,13 +2,7 @@ package tool
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import tool.dao.TestCommitProvider
-
-@Configuration
-@Import(ModelConfiguration::class, TestDaoConfiguration::class)
-class TestToolConfiguration {
-}
 
 @Configuration
 class TestDaoConfiguration {
